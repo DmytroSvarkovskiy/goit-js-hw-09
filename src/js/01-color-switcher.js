a70b3730f90db0c5b3754f49da846688b0d6a095
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-09-28 09:53:17 +0300

    did 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const off= document.querySelector('[data-stop]');
 
+const onStartClick = () => {
+    start.setAttribute('disabled', null);
+    timerId = setInterval(()=>{
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+const onOffClick=()=>{
+    clearInterval(timerId);
+    start.removeAttribute('disabled');
+}
+
+start.addEventListener('click', onStartClick);
+off.addEventListener('click', onOffClick);
