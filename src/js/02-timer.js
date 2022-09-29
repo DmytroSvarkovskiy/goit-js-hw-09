92a9df5ee3c8557eac2565728fd013efc21ceb59
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-09-29 19:45:00 +0300

    did 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -18,46 +18,44 @@ const options = {
     time_24hr: true,
     defaultDate: Date.now(),
     minuteIncrement: 1,
-  
     onClose(selectedDates) {
       console.log(selectedDates[0]);
       if (selectedDates[0] < Date.now()) {
           start.setAttribute('disabled', null) & Notiflix.Notify.failure("Please choose a date in the future");
       }
-        else start.removeAttribute('disabled'); 
+        else start.removeAttribute('disabled');  
+    }, 
+};
+const dataPickr = new flatpickr(input, options);
 
-        function convertMs(ms) {
-            ms = selectedDates[0] - Date.now();
-            const second = 1000;
-            const minute = second * 60;
-            const hour = minute * 60;
-            const day = hour * 24;
-
-            const days = Math.floor(ms / day);
-            const hours = Math.floor((ms % day) / hour);
-            const minutes = Math.floor(((ms % day) % hour) / minute);
-            const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-            return { days, hours, minutes, seconds }
-        };
-        function addLeadingZero(value) {
-           return String(value).padStart(2,0)
-        };
-        function onStartClick() {
-            start.setAttribute('disabled', null);
+const convertMs=ms=> {
+        ms = dataPickr.selectedDates[0] - Date.now();
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+        const days = Math.floor(ms / day);
+        const hours = Math.floor((ms % day) / hour);
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+        return { days, hours, minutes, seconds }
+};
+        
+const addLeadingZero=value=> {
+        return String(value).padStart(2,0)};
+        
+const onStartClick=()=>{
+        start.setAttribute('disabled', null);
             
-            timerId = setInterval(() => {
-                const timeTo = convertMs();
-                const { days, hours, minutes, seconds } = timeTo;
-                if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) { clearInterval(timerId) };
-                daysScr.textContent = addLeadingZero(days);
-                hoursScr.textContent = addLeadingZero(hours);
-                minutesScr.textContent = addLeadingZero(minutes);
-                secondsScr.textContent = addLeadingZero(seconds);
-            }, 1000);
+        timerId = setInterval(() => {
+        const timeTo = convertMs();
+        const { days, hours, minutes, seconds } = timeTo;
+            if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0)
+            { clearInterval(timerId) }       
+        daysScr.textContent = addLeadingZero(days);
+        hoursScr.textContent = addLeadingZero(hours);
+        minutesScr.textContent = addLeadingZero(minutes);
+        secondsScr.textContent = addLeadingZero(seconds);
+        }, 1000);
         };
         start.addEventListener('click', onStartClick);
-    },
-    
-};
-flatpickr(input, options);
-
