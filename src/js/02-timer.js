0f9bf5958fdd4062102d1cd8f89e31d3a487f895
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-09-29 10:59:27 +0300

    add 2 task

---
+++ src/js/02-timer.js
@@ -0,0 +1,63 @@
+import Notiflix from 'notiflix';
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const start = document.querySelector('[data-start]');
+const input = document.querySelector('#datetime-picker');
+const daysScr = document.querySelector('[data-days]');
+const hoursScr = document.querySelector('[data-hours]');
+const minutesScr = document.querySelector('[data-minutes]');
+const secondsScr = document.querySelector('[data-seconds]');
+start.setAttribute('disabled', null);
+let timerId = null;
+
+const options = {
+    enableTime: true,
+    enabledata: false,
+    time_24hr: true,
+    defaultDate: Date.now(),
+    minuteIncrement: 1,
+  
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      if (selectedDates[0] < Date.now()) {
+          start.setAttribute('disabled', null) & Notiflix.Notify.failure("Please choose a date in the future");
+      }
+        else start.removeAttribute('disabled'); 
+
+        function convertMs(ms) {
+            ms = selectedDates[0] - Date.now();
+            const second = 1000;
+            const minute = second * 60;
+            const hour = minute * 60;
+            const day = hour * 24;
+
+            const days = Math.floor(ms / day);
+            const hours = Math.floor((ms % day) / hour);
+            const minutes = Math.floor(((ms % day) % hour) / minute);
+            const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+            return { days, hours, minutes, seconds }
+        };
+        function addLeadingZero(value) {
+           return String(value).padStart(2,0)
+        };
+        function onStartClick() {
+            start.setAttribute('disabled', null);
+            
+            timerId = setInterval(() => {
+                const timeTo = convertMs();
+                const { days, hours, minutes, seconds } = timeTo;
+                if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) { clearInterval(timerId) };
+                daysScr.textContent = addLeadingZero(days);
+                hoursScr.textContent = addLeadingZero(hours);
+                minutesScr.textContent = addLeadingZero(minutes);
+                secondsScr.textContent = addLeadingZero(seconds);
+            }, 1000);
+        };
+        start.addEventListener('click', onStartClick);
+    },
+    
+};
+flatpickr(input, options);
+
