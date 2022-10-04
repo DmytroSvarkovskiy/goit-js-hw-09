03d23b0da4489d6e40572e7261206455347bf980
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-10-04 09:30:25 +0300

    частково зробив 3 завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,40 @@
 import Notiflix from 'notiflix';
+const form=document.querySelector('.form')
+const firstDelay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+let startStep = 0;
 
 
 function createPromise(position, delay) {
+  position = startStep;
+  delay = firstDelay.value;
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    
-  } else {
-    
-  }
-}
-
-const submit = document.querySelector('[type="submit"]');
-const form=document.querySelector('.form')
-console.log();
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } reject
+          ({ position, delay });
+       }, delay); 
+    }
+   )} 
+ 
 
 const onSubmitClick = event => {
   event.preventDefault();
-  
-}
-submit.addEventListener('click', onSubmitClick )
+   for (i = 0; i < amount.value; i += 1) {
+      startStep += 1; 
+   
+    createPromise().then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }).catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        })
+    }
+  startStep = 0;
+} 
+    
+
+form.addEventListener('submit', onSubmitClick)
