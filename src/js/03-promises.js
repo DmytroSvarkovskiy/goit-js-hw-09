ea9079589de6d4bdbb9800d2dd38fcce5e4b6448
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-10-05 00:00:08 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,32 +1,72 @@
+
 import Notiflix from 'notiflix';
 const form=document.querySelector('.form')
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
     return new Promise((resolve, reject) => {
-        if (shouldResolve) {
+      setTimeout(() => { if (shouldResolve) {
           resolve({ position, delay });
         } reject
-          ({ position, delay });
+          ({ position, delay })}, delay)
     }
    )} 
  
   const onSubmitClick = event => {
     event.preventDefault();
     const stepValue = Number(document.querySelector('[name="step"]').value);
-    const delay = Number(document.querySelector('[name="delay"]').value);
+    const delayValue = Number(document.querySelector('[name="delay"]').value);
     const amount = Number(document.querySelector('[name="amount"]').value)
-    let delayOver = delay;
+    let delayOver = delayValue;
     
     for (let i = 0; i < amount; i += 1) {
     const position = i+1 ;
-   setTimeout(() => { createPromise(position ,delay).then(({ position, delay }) => {
+     createPromise(position ,delayOver).then(({ position, delay }) => {
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       }).catch(({ position, delay }) => {
           Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
      })
-   }, delayOver); delayOver+= stepValue;
+   ; delayOver+=stepValue;
     }
 } 
   
 form.addEventListener('submit', onSubmitClick)
+
+
+
+
+
+
+
+// import Notiflix from 'notiflix';
+// const form=document.querySelector('.form')
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//     return new Promise((resolve, reject) => {
+//         if (shouldResolve) {
+//           resolve({ position, delay });
+//         } reject
+//           ({ position, delay });
+//     }
+//    )} 
+ 
+//   const onSubmitClick = event => {
+//     event.preventDefault();
+//     const stepValue = Number(document.querySelector('[name="step"]').value);
+//     const delay = Number(document.querySelector('[name="delay"]').value);
+//     const amount = Number(document.querySelector('[name="amount"]').value)
+//     let delayOver = delay;
+    
+//     for (let i = 0; i < amount; i += 1) {
+//     const position = i+1 ;
+//    setTimeout(() => { createPromise(position ,delay).then(({ position, delay }) => {
+//         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       }).catch(({ position, delay }) => {
+//           Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//      })
+//    }, delayOver); delayOver+= stepValue;
+//     }
+// } 
+  
+// form.addEventListener('submit', onSubmitClick)
