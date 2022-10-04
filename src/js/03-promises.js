6c021c38d5812436d87a5fdbc03f28a9e5d1e9a0
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-10-04 12:45:44 +0300

    add 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,40 +1,32 @@
 import Notiflix from 'notiflix';
 const form=document.querySelector('.form')
-const firstDelay = document.querySelector('[name="delay"]');
-const step = document.querySelector('[name="step"]');
 const amount = document.querySelector('[name="amount"]');
 
-let startStep = 0;
-
-
 function createPromise(position, delay) {
-  position = startStep;
-  delay = firstDelay.value;
   const shouldResolve = Math.random() > 0.3;
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
         if (shouldResolve) {
           resolve({ position, delay });
         } reject
           ({ position, delay });
-       }, delay); 
     }
    )} 
  
-
-const onSubmitClick = event => {
-  event.preventDefault();
-   for (let i = 0; i < amount.value; i += 1) {
-      startStep += 1; 
-   
-    createPromise().then(({ position, delay }) => {
+  const onSubmitClick = event => {
+    event.preventDefault();
+    const stepValue = Number(document.querySelector('[name="step"]').value);
+    const delay = Number(document.querySelector('[name="delay"]').value);
+    let delayOver = delay;
+    
+    for (let i = 0; i < amount.value; i += 1) {
+    const position = i+1 ;
+   setTimeout(() => { createPromise(position ,delay).then(({ position, delay }) => {
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       }).catch(({ position, delay }) => {
           Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        })
+     })
+   }, delayOver); delayOver+= stepValue;
     }
-  startStep = 0;
 } 
-    
-
+  
 form.addEventListener('submit', onSubmitClick)
